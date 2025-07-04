66d363d5a8dbb619ef0735d310c504c45afe6fbe
Christian Stewart <christian@aperture.us>
2025-07-03 19:27:08 -0700

    chore: format code
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- embed.go
+++ embed.go
@@ -2,5 +2,9 @@ package quickjswasi
 
 import _ "embed"
 
+// QuickJSWASM contains the binary contents of qjs-wasi.wasm.
+//
+// See: https://github.com/quickjs-ng/quickjs/releases/download/v0.10.1/qjs-wasi.wasm
+//
 //go:embed qjs-wasi.wasm
 var QuickJSWASM []byte
--- version.go
+++ version.go
@@ -4,7 +4,7 @@ package quickjswasi
 const (
 	// Version is the QuickJS-NG version
 	Version = "v0.10.1"
-	
+
 	// DownloadURL is the URL where this WASM file was downloaded from
 	DownloadURL = "https://github.com/quickjs-ng/quickjs/releases/download/v0.10.1/qjs-wasi.wasm"
 )
