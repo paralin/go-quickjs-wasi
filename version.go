169035e636cd546e33e29fc72a93491be22803b0
Christian Stewart <christian@aperture.us>
2025-11-24 14:20:21 -0800

    feat: update quickjs
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- version.go
+++ version.go
@@ -3,8 +3,7 @@ package quickjswasi
 // QuickJS-NG version information
 const (
 	// Version is the QuickJS-NG version
-	Version = "v0.10.1"
-
+	Version = "v0.11.0"
 	// DownloadURL is the URL where this WASM file was downloaded from
-	DownloadURL = "https://github.com/quickjs-ng/quickjs/releases/download/v0.10.1/qjs-wasi.wasm"
+	DownloadURL = "https://github.com/quickjs-ng/quickjs/releases/download/v0.11.0/qjs-wasi.wasm"
 )
